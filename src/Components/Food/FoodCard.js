e782f3e89156b049d2de8c184a33271546fc7913
Rahel <rahel.schneider99@gmail.com>
2025-01-10 12:45:58 +0100

    Layouted Food Page

--- src/Components/Food/FoodCard.js
+++ src/Components/Food/FoodCard.js
@@ -1,30 +1,32 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
+import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Chip from '@mui/material/Chip';
 import Rating from '@mui/material/Rating';
+import { Margin } from '@mui/icons-material';
 
 export default function MediaCard({ image, title, text, onShare, rating, category }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
-        sx={{ height: 140 }}
+        sx={{ height: 240 }}
         image={image}
         title={title}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {title}
+          <Chip label={category} size="small" variant="outlined" style={{marginLeft: "1rem"}}/>
         </Typography>
-        <Chip label={category} size="small" variant="outlined"/>
+        <Rating name="read-only" value={rating} readOnly style={{marginButton: "1rem"}}/>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
           {text}
         </Typography>
-        <Rating name="read-only" value={rating} readOnly />
       </CardContent>
       <CardActions>
         <Button size="small" onClick={onShare}>Get direction!</Button>
